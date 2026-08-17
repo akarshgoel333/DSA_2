de786839bcafd3edbe343f62934a6bd3f03b5201
akarshgoel333 <akarshgoel333@gmail.com>
2026-08-17 18:33:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Iterative Postorder/iterative-postorder.cpp
+++ Difficulty: Medium/Iterative Postorder/iterative-postorder.cpp
@@ -8,16 +8,28 @@ class Solution {
   public:
     vector<int> postOrder(Node* node) {
         // code here
-        stack<Node*>st;
         vector<int>ans;
-        st.push(node);
-        Node* temp;
-        while(!st.empty()){
-            temp = st.top();
-            st.pop();
-            ans.push_back(temp->data);
-            if(temp->left) st.push(temp->left);
-            if(temp->right) st.push(temp->right);
+        Node* curr;
+        while(node){
+            if(!node->right){
+                ans.push_back(node->data);
+                node = node->left;
+            }
+            else{
+                curr = node->right;
+                while(curr->left && curr->left!=node){
+                    curr = curr->left;
+                }
+                if(!curr->left){
+                    ans.push_back(node->data);
+                    curr->left = node;
+                    node = node->right;
+                }
+                else{
+                    curr->left = NULL;
+                    node = node->left;
+                }
+            }
         }
         reverse(ans.begin(),ans.end());
         return ans;
